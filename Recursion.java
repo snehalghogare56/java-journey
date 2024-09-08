d717726b98308487d4178ee071f6d26f0f925e28
snehalghogare56 <ghogaresnehal0606@gmail.com>
2024-09-08 16:12:01 +0530

    java files added

--- Recursion.java
+++ Recursion.java
@@ -93,12 +93,56 @@ public class Recursion {
         }
         return halfsq;
     }*/
+    // public static int tiling(int n){
+    //     if(n == 0 || n==1){
+    //         return 1;
+    //     }
+
+    //     //vertical
+    //     int vertical=tiling(n-1);
+    //     //horizontal
+    //     int horizontal=tiling(n-2);
+
+    //     int total_ways=vertical + horizontal;
+    //     return total_ways;
+
+    // }
+
+    // public static void duplicates(String str,int idx, StringBuilder sb, boolean map[]){
+    //     if(idx==str.length()){
+    //         System.out.println(sb);
+    //         return;
+    //     }
+    //     char currchar=str.charAt(idx);
+    //     if(map[currchar -'a'] == true){
+    //         duplicates(str, idx+1, sb, map);
+    //     }else{
+    //         map[currchar -'a']=true;
+    //         duplicates(str, idx+1, sb.append(currchar), map);
+    //     }
+
+    // }
+
+    public static int pairing(int n){
+        if(n == 1 || n==2){
+            return n;
+        }
+        // int single=pairing(n-1);
+        // int Double= pairing(n-2);
+        // int pairways=(n-1) * Double;
+        // int total= single + pairways
+        // ;
+        // return total;
+        return pairing(n-1) + (n-1) * pairing(n-2);
+
+
+    }
 
     
 
 
     public static void main(String[] args) {
-        //int n=5;
+        int n=3;
         //printDec(n);
        // printInc(n);
        //System.out.println(fact(n));
@@ -110,7 +154,11 @@ public class Recursion {
        //System.out.println(sorted(arr, i));
        //System.out.println(finf(arr, i, key));
        //System.out.println(findl(arr, key, i));
-       System.out.println(pow(2, 10));
+       //System.out.println(pow(2, 10));
+       //System.out.println(tiling(n));
+    //    String str="apnancollege";
+    //    duplicates(str, 0, new StringBuilder(""), new boolean[26]);
+    System.out.println(pairing(n));
 
     }
 
